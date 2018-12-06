fbecf34d6ea7949c1ac441eefa7ad6fb9a988d77
Silvester98 <1539168414@qq.com>
2018-12-06 10:30:47 +0800

    dm ok

--- README.md
+++ README.md
@@ -203,4 +203,39 @@ endmodule
 ```
 正常工作。
 
+### dm module
+测试代码
+```
+`timescale 1ns / 1ps
+module testbench();
+    reg clk;
+    reg rst;
+    
+    reg[31:0] addr;
+    reg[31:0] wdata;
+    reg wen;
+    
+    wire[31:0] dout;
+    dm dm(.addr(addr[11:2]), .wdata(wdata), .w_en(wen), .clk(clk), .dout(dout));
+    
+    initial begin
+        rst = 1;
+        clk = 0;
+        $readmemh("/home/silvester/project/mysinglecycle/instfile.txt", dm.dm);
+        addr = 32'h00000004;
+        wdata = 32'h00abcdef;
+        wen = 0;
+        # 10 addr = 32'h00000018;
+        wen = 1;
+        #30
+        $stop;
+    end
+    
+    always
+        #20 clk = ~clk; // colck
+endmodule
+
+```
+正常工作。
+
 具体各模块代码详见 srcs 目录下文件。
--- mysinglecycle.srcs/sim_1/new/testbench.v
+++ mysinglecycle.srcs/sim_1/new/testbench.v
@@ -3,31 +3,23 @@ module testbench();
     reg clk;
     reg rst;
     
-    reg[31:0] A,B;
-    wire[31:0] C;
-    reg[4:0] s;
-    reg[`ALU_CTRL-1:0] alu_ctrl;
-    wire beqout;
+    reg[31:0] addr;
+    reg[31:0] wdata;
+    reg wen;
     
-    alu alu(.A(A), .B(B), .alu_ctrl(alu_ctrl), .s(s), .C(C), .beqout(beqout));
+    wire[31:0] dout;
+    dm dm(.addr(addr[11:2]), .wdata(wdata), .w_en(wen), .clk(clk), .dout(dout));
+    
     initial begin
         rst = 1;
         clk = 0;
-        A = 32'h001f;
-        B = 32'h0001;
-        s = 5'h3;
-        alu_ctrl = 3'b000;
-        #10
-        B = 32'h001f;
-        alu_ctrl = 3'b001;
-        #10
-        B = 32'h002a;
-        alu_ctrl = 3'b100;
-        #10
-        alu_ctrl = 3'b101;
-        #10
-        alu_ctrl = 3'b010;
-        #10
+        $readmemh("/home/silvester/project/mysinglecycle/instfile.txt", dm.dm);
+        addr = 32'h00000004;
+        wdata = 32'h00abcdef;
+        wen = 0;
+        # 10 addr = 32'h00000018;
+        wen = 1;
+        #30
         $stop;
     end
     
