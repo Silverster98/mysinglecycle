b75ace356f8d712ad6bf93fe66843b0cfad75d53
Silvester98 <1539168414@qq.com>
2018-12-06 09:09:42 +0800

    mux ok

--- README.md
+++ README.md
@@ -87,4 +87,40 @@ endmodule
 ```
 工作正常。
 
+### mux module
+测试代码
+```
+`timescale 1ns / 1ps
+module testbench();
+    reg clk;
+    reg rst;
+    
+    reg[1:0] sel;
+    reg[4:0] in1,in2,in3,in4;
+    wire[4:0] out;
+    
+    mux_4_5 mux(.in1(in1), .in2(in2), .in3(in3), .sel(sel), .out(out));
+
+    initial begin
+        rst = 1;
+        clk = 0;
+        in1 = 5'b00001;
+        in2 = 5'b00010;
+        in3 = 5'b00011;
+        in4 = 5'b00100;
+        sel = 2'b00;
+        #10 sel = 2'b01;
+        #10 sel = 2'b10;
+        #10 sel = 2'b11;
+        #10;
+        
+        $stop;
+    end
+    
+    always
+        #20 clk = ~clk; // colck
+endmodule
+
+```
+正常工作，其他两个mux按类似方式检验。
 具体各模块代码详见 srcs 目录下文件。
--- mysinglecycle.srcs/sim_1/new/testbench.v
+++ mysinglecycle.srcs/sim_1/new/testbench.v
@@ -3,32 +3,25 @@ module testbench();
     reg clk;
     reg rst;
     
-    reg[15:0] imm_16;
-    reg[1:0]  ext_op;
-    wire[31:0] extout;
-    extend ext(.imm_16(imm_16), .ext_op(ext_op), .dout(extout));
+    reg[1:0] sel;
+    reg[4:0] in1,in2,in3,in4;
+    wire[4:0] out;
+    
+    mux_4_5 mux(.in1(in1), .in2(in2), .in3(in3), .sel(sel), .out(out));
 
     initial begin
         rst = 1;
         clk = 0;
-        
-        ext_op = 2'b00;
-        imm_16 = 16'h0017;
-        #10 ext_op = 2'b01;
-        #10 ext_op = 2'b10;
-        
+        in1 = 5'b00001;
+        in2 = 5'b00010;
+        in3 = 5'b00011;
+        in4 = 5'b00100;
+        sel = 2'b00;
+        #10 sel = 2'b01;
+        #10 sel = 2'b10;
+        #10 sel = 2'b11;
         #10;
-        ext_op = 2'b00;
-        imm_16 = 16'hf113;
-        #10 ext_op = 2'b01;
-        #10 ext_op = 2'b10;
         
-        #10;
-        ext_op = 2'b00;
-        imm_16 = 16'h0000;
-        #10 ext_op = 2'b01;
-        #10 ext_op = 2'b10;
-        #10;
         $stop;
     end
     
