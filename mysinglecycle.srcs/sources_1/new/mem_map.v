115a403d47bdb13b39872d3b7e9b6c6a10fafe46
Silvester98 <1539168414@qq.com>
2018-12-30 11:10:14 +0800

    add mem_map.v

---
+++ mysinglecycle.srcs/sources_1/new/mem_map.v
@@ -0,0 +1,20 @@
+`timescale 1ns / 1ps
+
+module mem_map(
+    input wire[31:0] w_addr_i,
+    
+    output wire ram_w_en,
+    output wire io_w_en,
+    output wire[31:0] w_addr_o
+    );
+    // 0x00000000~0x0fffffff for ram 256M
+    // 0x10000000~0x1fffffff for io  256M
+    
+    wire[3:0] sel;
+    
+    assign sel = w_addr_i[31:28];
+    assign ram_w_en = (sel == 4'b0000) ? 1 : 0;
+    assign io_w_en = (sel == 4'b0001) ? 1 : 0;
+    assign w_addr_o = {16'b0,w_addr_i[15:0]};
+    
+endmodule
